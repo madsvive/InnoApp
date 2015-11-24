0f4d53bf6f32d69bcec6db424bea18c8d1890c74
Mads Vive <madsvive@gmail.com>
2015-11-24 15:58:53 +0100

    Dummy data, search function included (not working)
    Search function not working yet..

--- www/js/data.js
+++ www/js/data.js
@@ -4,18 +4,37 @@
 
 angular.module('starter.data', [])
 
+
+/**/
 .factory('CompanyList', function () {
 
     var companyList = [
         {
+            name: "AG Solutions",
+            logo: "img/ag.jpg",
+            category: "IT",
+            subscription: "CBS Certified",
+            shortsummary: "Ønsker du din virksomhed på nettet? Så er du i trygge hænder hos AG-solutions...",
+            longsummary: "Ønsker du din virksomhed på nettet? Så er du i trygge hænder hos AG-solutions. Vi tilbyder komplette webløsninger, skræddersyet efter dine behov og ønsker. AG-solutions startede i 2015 med en vision om at kunne levere komplette webløsninger til virksomheder i alle størrelser. Med en webside kan du reklamere for din forretning på en visuel & begribende måde, ved blot at have din smartphone ved hånden.",
+            follow: 2,
+            funding: "2.000.000 for 10%",
+            goal: "1.200.000/2.000.000",
+            team: ["Akan", "Mads", "Omar"],
+            teamimg: /img/,
+            budget: /budgets/,
+            url: "http://www.virksomhed.dk/",
+            facebook: "facebook.com/mads.vive",
+            linkedin: "linkedin.com/mads.vive"
+        }, {
             name: "Taxa fyn A/S",
             logo: "img/taxa.png",
             category: "Transport",
-            subscription: "free",
+            subscription: "None",
             shortsummary: "Drive is a full-stack e-commerce platform for new car buyers and dealers, with backing from the top franchised dealerships across California...",
             longsummary: "Drive is a full-stack e-commerce platform for new car buyers and dealers, with backing from the top franchised dealerships across California. For new car buyers, Drive is the first and only technology platform that lets you complete the entire process online, with over 10,000 new cars in stock, direct online financing, leasing, and trade-ins, plus *free* delivery to your home or office. For franchised new car dealerships, Drive is a full-stack e-commerce solution that turbocharges the entire business: retail, F&I, trade-in, and service.",
             follow: 4,
             funding: "200.000 for 20%",
+            goal: "20.000/200.000",
             team: ["Akan", "Mads", "Omar"],
             teamimg: /img/,
             budget: /budgets/,
@@ -31,7 +50,8 @@ angular.module('starter.data', [])
             shortsummary: "Imagine a tool that enabled the business owner or marketing consultant to manage their entire marketing efforts in one location, this is exactly what 123 Opstart is developing....",
             longsummary: "Imagine a tool that enabled the business owner or marketing consultant to manage their entire marketing efforts in one location, this is exactly what 123 Opstart is developing. This new tool will include a wide range of videos, podcasts, work flows, checklists and learning methodologies to allow either a business owner, marketing director or consultant to run their entire marketing effort from the one portal. It will include dashboards, planning tools, people management, marketing plans, case studies, reporting templates, and marketing training. Already there are 5 major organisations wanting this tool in their business, and Hunter has identified key people to deliver IT, architecture and high end production qualities for the contents. ",
             follow: 0,
-            funding: "200.000 for 5%",
+            funding: "100.000 for 50%",
+            goal: "110.000/100.000",
             team: ["Akan", "Mads", "Omar"],
             teamimg: /img/,
             budget: /budgets/,
@@ -46,22 +66,8 @@ angular.module('starter.data', [])
             shortsummary: "We are a small artisan sourdough bakery, based in Levenshulme, Manchester, with a passion for beautiful food and sourdough bread!...",
             longsummary: "We are a small artisan sourdough bakery, based in Levenshulme, Manchester, with a passion for beautiful food and sourdough bread! We now have the opportunity to expand the cafe to make at least 20 more seats and create a fantastic cooking space. We also want to utilise the small outdoor area we have to make a lovely light, family friendly space for people to enjoy sitting in all seasons. We'll be using a local Levenshulme architect and creating more jobs for Manchester folk. We also plan to apply for an alcohol license to extend our opening times into the evening to provide a space to sit and drink, using local ales and organic wines.",
             follow: 7,
-            funding: "200.000 for 5%",
-            team: ["Akan", "Mads", "Omar"],
-            teamimg: /img/,
-            budget: /budgets/,
-            url: "http://www.virksomhed.dk/",
-            facebook: "facebook.com/mads.vive",
-            linkedin: "linkedin.com/mads.vive"
-        }, {
-            name: "AG Solutions",
-            logo: "img/ag.jpg",
-            category: "IT",
-            subscription: "Certified",
-            shortsummary: "Ønsker du din virksomhed på nettet? Så er du i trygge hænder hos AG-solutions...",
-            longsummary: "Ønsker du din virksomhed på nettet? Så er du i trygge hænder hos AG-solutions. Vi tilbyder komplette webløsninger, skræddersyet efter dine behov og ønsker. AG-solutions startede i 2015 med en vision om at kunne levere komplette webløsninger til virksomheder i alle størrelser. Med en webside kan du reklamere for din forretning på en visuel & begribende måde, ved blot at have din smartphone ved hånden.",
-            follow: 2,
-            funding: "200.000 for 5%",
+            funding: "300.000 for 5%",
+            goal: "80.000/300.000",
             team: ["Akan", "Mads", "Omar"],
             teamimg: /img/,
             budget: /budgets/,
@@ -72,11 +78,12 @@ angular.module('starter.data', [])
             name: "Angellist",
             logo: "img/angellist.png",
             category: "Crowdinvestment",
-            subscription: "CBS Certified",
+            subscription: "DTU Certified",
             shortsummary: "...",
             longsummary: ".......",
             follow: 4,
-            funding: "200.000 for 5%",
+            funding: "600.000 for 49%",
+            goal: "20.000/600.000",
             team: ["Akan", "Mads", "Omar"],
             teamimg: /img/,
             budget: /budgets/,
