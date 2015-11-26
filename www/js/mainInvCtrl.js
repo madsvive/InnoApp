f96be5b1c844aedacef1d79c3f8349057c5829be
Mads Vive <madsvive@gmail.com>
2015-11-26 14:48:17 +0100

    Unnessecary company pages deleted

--- www/js/mainInvCtrl.js
+++ www/js/mainInvCtrl.js
@@ -7,21 +7,27 @@ angular.module('starter.mainInvCtrl', [])
 .controller('mainInvCtrl', function ($scope, $state) {
 
             $scope.card = function (id) {
+                $scope.state = 0;
                 var akan = id;
                 
                 if (akan === 1) {
+                    $scope.state = 1;
                     $state.go('company1')
                 }
                 if (akan === 2) {
+                    $scope.state = 2;
                     $state.go('company2')
                 }
                 if (akan === 3) {
+                    $scope.state = 3;
                     $state.go('company3')
                 }
                 if (akan === 4) {
+                    $scope.state = 4;
                     $state.go('company4')
                 }
                 if (akan === 5) {
+                    $scope.state = 5;
                     $state.go('company5')
                 }
                 else {
